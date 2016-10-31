5ce508940fefa08cbd8498446da592d5cf07d3ab
Christophe Hurpeau <christophe@evaneos.com>
2016-10-31 15:05:39 +0100

    fix lint

--- src/server/middlewares/renderer.js
+++ src/server/middlewares/renderer.js
@@ -1,11 +1,14 @@
+/* global ASSETS_BY_CHUNK_NAME */
 import render from '../render';
 import config from '../../../config';
 
 export default () => function* rendererMiddleware() {
     const { renderProps, store } = this.state;
-    const mainEntry =
-        // eslint-disable-next-line no-undef
-        (ASSETS_BY_CHUNK_NAME || this.res.locals.webpackStats.toJson().assetsByChunkName)[config.client.name];
-    this.body =
-        yield render(renderProps, store, Array.isArray(mainEntry) ? mainEntry[0] : mainEntry);
+    const assetsByChunkName = (
+      ASSETS_BY_CHUNK_NAME || this.res.locals.webpackStats.toJson().assetsByChunkName
+    );
+    let mainEntry = assetsByChunkName[config.client.name];
+    if (Array.isArray(mainEntry)) mainEntry = mainEntry[0];
+
+    this.body = yield render(renderProps, store, mainEntry);
 };
